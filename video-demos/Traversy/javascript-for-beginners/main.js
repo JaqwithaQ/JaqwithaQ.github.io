02f1a293227cce2a66376069a9224dd46f806775
Jackie <jboden@gmail.com>
2020-11-23 15:07:46 -0800

    Favicon and some java tutorials

---
+++ video-demos/Traversy/javascript-for-beginners/main.js
@@ -0,0 +1,41 @@
+alert('hello world');
+console.log('hello world');
+
+//variables: let(can reassign values), const(short for constant, can't be reassigned)
+
+//data types
+
+//primitive data types: directly assigned to memory
+
+//string, numbers, boolean, null, undefined, symbol
+
+//string
+
+const name = 'John';
+
+
+//number
+
+const age = 30;
+
+//boolean
+
+// const isCool = true;
+
+//decimal
+
+// const rating = 4.5;
+
+//null
+
+// const x = null
+
+//undefined
+
+// const y = undefined;
+// let z
+
+// console.log(typeof isCool)
+
+// Concatenation
+console.log("")
